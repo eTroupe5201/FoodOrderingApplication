35ab27de2ed97689acee12c1728e2ca933753416
eTroupe5201 <100246757+eTroupe5201@users.noreply.github.com>
2024-02-20 04:55:12 -0600

    Attempting To Fix Frontend Of Hosted Site

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -6,21 +6,21 @@ import {motion} from "framer-motion";
 export function ImageSlider() {
   const images = [
     {
-      url: "src/assets/Slider/TableSetting.jpg",
+      url: "https://groupprojectdepaul.s3.us-east-2.amazonaws.com/assets/Slider/TableSetting.jpg",
       heading: "Welcome to Divine Delicacies!",
       content: "Indulge Your Senses: A Gastronomic Journey through Our Culinary Creations! From savory starters to delectable desserts, every slide invites you to savor the flavors of Divine Delicacies. Bon appétit!",
       button: "Order Now",
       link: "/login"
     },
     {
-      url: "src/assets/Slider/TableSetting.jpg",
+      url: "https://groupprojectdepaul.s3.us-east-2.amazonaws.com/assets/Slider/shutterstock_333578117.jpg",
       heading: "Discover Our Exquisite Menu!",
       content: "From tantalizing appetizers to mouthwatering mains and indulgent desserts, each dish at Divine Delicacies is a culinary masterpiece, meticulously crafted to delight your senses. Join us and experience a symphony of flavors that will leave you craving more.",
       button: "Explore Menu",
       link: "/Menu"
     },
     {
-      url: "src/assets/Slider/TableSetting.jpg",
+      url: "https://groupprojectdepaul.s3.us-east-2.amazonaws.com/assets/Slider/shutterstock_588796445.jpg",
       heading: "Satisfy Your Cravings!",
       content: "Ready to tantalize your taste buds? Place your order now and embark on a culinary journey with us! Whether you are craving savory classics, indulgent treats, or something in between, we have  you covered. Click below to satisfy your cravings and experience a world of flavor delivered straight to your door.",
       button: "Order Here",
