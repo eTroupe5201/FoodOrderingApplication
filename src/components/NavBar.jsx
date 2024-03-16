6fee726698f6fd7269a8ae3b3291b3d96d35c757
eTroupe5201 <etroupe@depaul.edu>
2024-03-16 14:54:05 -0500

    Update Performance Attempt

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -85,7 +85,7 @@ export function NavBar() {
                     _hover={{ boxShadow: "0 0 10px 1px tan"}}
                     w={{  md: "18em" }}
                     maxH={{  md: "11em"}}
-                    className="photo" src="https://d1zh5cyxaugen.cloudfront.net/divineDelicaciesResize.png" /> 
+                    className="photo" src="https://d1zh5cyxaugen.cloudfront.net/divineDelicaciesResizedTiny.png" /> 
                     </Link> 
                 )}
 
