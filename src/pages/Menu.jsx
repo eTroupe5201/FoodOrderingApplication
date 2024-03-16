c252783bb7ade6dbd3154566bc5bbb2548e7634c
eTroupe5201 <100246757+eTroupe5201@users.noreply.github.com>
2024-03-15 19:28:27 -0500

    Revert "Erica's branch (#25)"
    This reverts commit a5c368ede81297bd31c1fcc8fce8e8ee28481cb4.

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,6 @@
 ///* eslint-disable no-unused-vars */
 ///* eslint-disable react/jsx-key */
 import React from "react";
-
 import { Center, Box, SimpleGrid, Heading, Image, Link, Text } from "@chakra-ui/react";
 import { useDataProvider } from "../components/dataProvider";
 import { useNavigate } from "react-router-dom";
@@ -20,7 +19,6 @@ export const Menu = () => {
    
     return (
         <> 
-
        <FilterNavigation   />
          {selectedFilter === "Search" && <Search />} 
 
