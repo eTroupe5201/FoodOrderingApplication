374e0f43894441dd012f59fb364c0a69f5df83e6
eTroupe5201 <etroupe@depaul.edu>
2024-03-19 21:35:41 -0500

    Peformance Update Fixing Layout Shift, Lazy Load Footer, Preload Images In Slider,

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,72 +1,63 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react"
-import {Center,Container,Heading, SimpleGrid, Box,  Image, Text, Grid} from "@chakra-ui/react"
+import { Box, Center, Container, Grid, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
-
 
 export function Footer() {
     return (
-     
-        <Box  role="navigation" className="Footer"  bg = "black"  paddingBottom={10}>
-           <section className="wave"></section>  
-            <Container> 
-                <SimpleGrid  bg = "black" color="white"columns={4} templateColumns={{base:"repeat(1, 1fr)", sm:"repeat(2, 1fr)",lg:"repeat(4, 1fr)" }}>
-    
-                    <Box colSpan={{base: 1, md:4,  xl: 4}}>
-                        <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }}  fontFamily="'Raleway', sans-serif " mb={5} mt={10}>About Us</Heading>
-                    
-                    <Center><Image width={{base:"50px"}} alt= "Favicon" height={{base:"50px"}}src="https://d1zh5cyxaugen.cloudfront.net/Favicon.png"></Image></Center>
-            
-                    <Text fontSize={{ base: "12px",md: "15px", lg: "18px"}} mt={5}>
-                        Discover culinary excellence at Devine Delicacies. We are passionate about crafting 
-                        extraordinary flavors and providing a memorable dining experience. From our kitchen to
-                        your table, savor the essence of exceptional cuisine with us. Welcome to Delicious Delicacies, 
-                        where every bite tells our story.</Text>
+        <Box role="navigation" className="Footer" bg="black" paddingBottom={10}>
+            <section className="wave"></section>
+            <Container>
+                <SimpleGrid bg="black" color="white" columns={4} templateColumns={{ base: "repeat(1, 1fr)", sm: "repeat(2, 1fr)", lg: "repeat(4, 1fr)" }} spacing={5}>
+
+                    <Box colSpan={{ base: 1, md: 4, xl: 4 }}>
+                        <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }} fontFamily="'Raleway', sans-serif" mb={5} mt={10}>About Us</Heading>
+                        <Center><Image width="50px" alt="Favicon" height="50px" src="https://d1zh5cyxaugen.cloudfront.net/Favicon.png"></Image></Center>
+                        <Text fontSize={{ base: "12px", md: "15px", lg: "18px" }} mt={5}>
+                            Discover culinary excellence at Devine Delicacies. We are passionate about crafting
+                            extraordinary flavors and providing a memorable dining experience. From our kitchen to
+                            your table, savor the essence of exceptional cuisine with us. Welcome to Delicious Delicacies,
+                            where every bite tells our story.
+                        </Text>
                     </Box>
 
-                    <Box colSpan={{base: 1,  md:4,  xl: 4}}>
-                        <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }}
-                        fontFamily="'Raleway', sans-serif "mb={5} mt={10}>Useful Links</Heading>
-                    
-                    <Grid fontSize={{ base: "12px",md: "15px", lg: "18px" }}>
-                <ul>
-                <li><Link to="/"><Text>Home</Text></Link></li>
-                <li><Link to="/contact"><Text >Contact Us</Text></Link></li>
-                <li><Link to="/menu"><Text >Order</Text></Link></li>
-                <li><Link to="/login"><Text >Login</Text></Link></li>
-                {/* <li><Link to="/contact"><Text>Contact</Text></Link></li> */}
-                {/* <li><Link to="/info"><Text>About</Text></Link></li> */}
-                </ul>
-                 
-                    </Grid>
-                    <Text mt={5}></Text>
+                    <Box colSpan={{ base: 1, md: 4, xl: 4 }}>
+                        <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }} fontFamily="'Raleway', sans-serif" mb={5} mt={10}>Useful Links</Heading>
+                        <Grid fontSize={{ base: "12px", md: "15px", lg: "18px" }}>
+                            <ul style={{ listStyleType: 'none', padding: 0 }}>
+                                <li><Link to="/"><Text>Home</Text></Link></li>
+                                <li><Link to="/contact"><Text>Contact Us</Text></Link></li>
+                                <li><Link to="/menu"><Text>Order</Text></Link></li>
+                                <li><Link to="/login"><Text>Login</Text></Link></li>
+                            </ul>
+                        </Grid>
                     </Box>
-                    <Box colSpan={{base: 1,  md:4,  xl: 4}}>
-                    <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }}fontFamily="'Raleway', sans-serif " mb={5} mt={10}>Follow Us</Heading>
-                    <Grid fontSize={{ base: "12px",md: "15px", lg: "18px"}}>
-                 <ul>
-                <li><Link to="/"><Text >Facebook</Text></Link></li>
-                <li><Link to="/"><Text >Instagram</Text></Link></li>
-                <li><Link to="/"><Text >Twitter</Text></Link></li>
-                <li><Link to="/"><Text >Youtube</Text></Link></li>
-                <li><Link to="/"><Text >TikTok</Text></Link></li>
-       </ul>
-                    </Grid>
+
+                    <Box colSpan={{ base: 1, md: 4, xl: 4 }}>
+                        <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }} fontFamily="'Raleway', sans-serif" mb={5} mt={10}>Follow Us</Heading>
+                        <Grid fontSize={{ base: "12px", md: "15px", lg: "18px" }}>
+                            <ul style={{ listStyleType: 'none', padding: 0 }}>
+                                <li><Link to="/"><Text>Facebook</Text></Link></li>
+                                <li><Link to="/"><Text>Instagram</Text></Link></li>
+                                <li><Link to="/"><Text>Twitter</Text></Link></li>
+                                <li><Link to="/"><Text>Youtube</Text></Link></li>
+                                <li><Link to="/"><Text>TikTok</Text></Link></li>
+                            </ul>
+                        </Grid>
                     </Box>
-                    <Box colSpan={{base: 1, md:4,  xl: 4}}>
-                        <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }}fontFamily="'Raleway', sans-serif " mb={5} mt={10}>Contact Us</Heading>
-                    <Grid fontSize={{ base: "12px",md: "15px", lg: "18px"}}>
-                        <Text paddingBottom={1} pt={2}>1234 Depaul Drive</Text>
-                        <Text paddingBottom={5} >Chicago, IL 32801</Text>
-                        <Text paddingBottom={5}> Phone: 407-555-5555</Text>
-                        <Text>Email: DevineDelicacies@gmail.com</Text>
-                    </Grid>
+
+                    <Box colSpan={{ base: 1, md: 4, xl: 4 }}>
+                        <Heading fontSize={{ base: "15px", md: "20px", lg: "30px" }} fontFamily="'Raleway', sans-serif" mb={5} mt={10}>Contact Us</Heading>
+                        <Grid fontSize={{ base: "12px", md: "15px", lg: "18px" }}>
+                            <Text paddingBottom={1} pt={2}>1234 Depaul Drive</Text>
+                            <Text paddingBottom={5}>Chicago, IL 32801</Text>
+                            <Text paddingBottom={5}>Phone: 407-555-5555</Text>
+                            <Text>Email: DevineDelicacies@gmail.com</Text>
+                        </Grid>
                     </Box>
-                    
-                
-                </SimpleGrid> 
-                </Container>
+
+                </SimpleGrid>
+            </Container>
         </Box>
-      
     );
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,6 +1,3 @@
-///* eslint-disable no-unused-vars */
-///* eslint-disable react/jsx-key */
-// Import necessary components and hooks
 import { Box, Center, Heading, Image, Link, SimpleGrid, Text } from "@chakra-ui/react";
 import { useDataProvider } from "../components/dataProvider";
 import { useNavigate } from "react-router-dom";
@@ -11,22 +8,16 @@ import { Type } from "../components/Type";
 import { DietaryNeeds } from "../components/DietaryNeeds";
 import { Search } from "../components/Search";
 
-// Define the maximum length for item descriptions
 const MAX_DESCRIPTION_LENGTH = 70;
-
 const itemImageWidth = "260px";
-const itemImageHeight = "173x";
+const itemImageHeight = "173px";
 
-// Define the Menu component
 export const Menu = () => {
-    // Retrieve necessary data and functions from the data provider hook
     const navigate = useNavigate();
     const { selectedFilter, categories, getItemsByCategory, user } = useDataProvider();
 
-
-    // Render the Menu component
     return (
-        <Box maxW="100%" mx="auto"> {/* Container with fixed width and centered */}
+        <Box maxW="100%" mx="auto">
             <Helmet>
                 <meta
                     name="Description"
@@ -34,7 +25,6 @@ export const Menu = () => {
                 />
             </Helmet>
 
-            {/* Render filter navigation based on selected filter */}
             <FilterNavigation />
             {selectedFilter === "Search" && <Search />}
             {selectedFilter === "Sort" && <Sort />}
@@ -42,19 +32,16 @@ export const Menu = () => {
             {selectedFilter === "Dietary" && <DietaryNeeds />}
             {(selectedFilter === "") && (
                 <Center>
-                    <Box data-test="MENU" title="menu-grid" className="MenuContainer" mt="30px" maxW="90%"
-                        justifyContent="center" p={5}>
+                    <Box alignItems="center" textAlign="center" mt="30px" maxW="90%" p={5}>
                         {categories.map((category) => (
-                            <Box data-test={`menu-items=${category.id}`} className="Menu" key={category.id} p={5} boxShadow="md" border="2px tan solid" width="100%">
+                            <Box key={category.id} p={5} boxShadow="md" border="2px tan outset" color="white" bg="black" borderRadius="25px" width="100%">
                                 <Center>
                                     <Box p={2} borderRadius="md" width="75%" mb={3}>
-                                        <Heading data-test={`map-item=${category.title}`} as="h2" fontFamily="'Great Vibes', cursive" padding="15px"
-                                            fontSize={{ base: "30px", md: "40px", lg: "50px" }} mb={1}>
+                                        <Heading as="h2" fontFamily="'Great Vibes', cursive" padding="15px" fontSize={{ base: "30px", md: "40px", lg: "50px" }} mb={1}>
                                             {category.title}
                                         </Heading>
                                         {category.description && (
-                                            <Text alt="category-description" padding="15px" fontSize={{ base: "12px", md: "13px", lg: "14px" }}
-                                                color="white">
+                                            <Text padding="15px" fontSize={{ base: "12px", md: "13px", lg: "14px" }} color="white">
                                                 {category.description}
                                             </Text>
                                         )}
@@ -64,33 +51,25 @@ export const Menu = () => {
                                     templateColumns={{ base: "repeat(1, 1fr)", sm: "repeat(2, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)", xl: "repeat(4, 1fr)" }}
                                     spacing={10}>
                                     {getItemsByCategory(category.id).map((item) => (
-                                        <Link alt="menu-link" key={item.id} data-test={`items-link=${item.id}`} href={`/item/${item.id}`} onClick={(e) => {
+                                        <Link key={item.id} href={`/item/${item.id}`} onClick={(e) => {
                                             e.preventDefault();
-                                            console.log(user); // 打印当前的user对象
                                             navigate(`/item/${item.id}`);
                                         }} _hover={{ textDecoration: "none" }}>
-                                            <Box alt="menu-item" colSpan={{ base: 1, sm: 1, xl: 4 }} borderRadius="25px" p={2}
-                                                justify="space-between" align="center" borderWidth="1px" borderColor="tan" 
-                                                padding="15px" _active={{ transform: "translateY(2px)", bg: "white", boxShadow: "inset  1px 1px 5px 2px rgba(210, 180, 140, 0.9)", backgroundImage: "linear-gradient(rgb(0 0 0/90%) 0 0)" }}
-                                                maxWidth={{ base: "100%", md: "100%", lg: "100%" }} width="100%" height="auto" minHeight="100%"
-                                                mb={2} _hover={{ boxShadow: "0 0 10px 1px tan" }}>
-                                                <a href={`/item/${item.id}`}>
-                                                    <Image alt="menu-image" data-test={`item-image=${item.image}`}
-                                                    src={item.image?.src} 
-                                                      //  src={item.image ? `https://d1zh5cyxaugen.cloudfront.net/resizedImages/${decodeURIComponent(item.label).replace(/%C3%A8/g, 'e').replace(/[\d.%]/g, '').replace(/\s+/g, '')}.jpg` : placeholderImageUrl}
-                                                        borderRadius="25px" width={itemImageWidth} height={itemImageHeight}
-                                                        size={{ base: "100%" }} objectFit="cover" mr={3} />
-                                                </a>
-                                                <Heading data-test={`item-label=${item.label}`} fontFamily="'Raleway', sans-serif" padding="20px" as="h3"
-                                                    fontSize={{ base: "12px", sm: "13px", md: "14px", lg: "15px", xl: "15px" }} >
+                                            <Box borderRadius="25px"  borderWidth="1px" borderColor="tan" padding="15px"
+                                                _active={{ transform: "translateY(2px)", bg: "white", boxShadow: "inset 1px 1px 5px 2px rgba(210, 180, 140, 0.9)" }}
+                                                _hover={{ boxShadow: "0 0 10px 1px tan" }}>
+                                                <Center>
+                                                    <Image ml="10px" alt="menu-photo" src={item.image?.src} borderRadius="25px" width={itemImageWidth} height={itemImageHeight} objectFit="cover" mr={3} />
+                                                </Center>
+                                                <Heading as="h3" fontFamily="'Raleway', sans-serif" padding="20px" fontSize={{ base: "12px", sm: "13px", md: "14px", lg: "15px", xl: "15px" }} >
                                                     {item.label}
                                                 </Heading>
-                                                <Text data-test={`item-description=${item.description}`} height="6m" maxHeight="6em" textOverflow="ellipsis" fontFamily="'Raleway', sans-serif"
-                                                    fontSize={{ base: "11px", sm: "12px", md: "13px", lg: "14px", xl: "15px" }}
-                                                    fontWeight="bold"> {item.description.length > MAX_DESCRIPTION_LENGTH
+                                                <Text height="6em" maxHeight="6em" overflow="hidden" textOverflow="ellipsis" fontFamily="'Raleway', sans-serif" fontSize={{ base: "11px", sm: "12px", md: "13px", lg: "14px", xl: "15px" }} fontWeight="bold">
+                                                    {item.description.length > MAX_DESCRIPTION_LENGTH
                                                         ? `${item.description.slice(0, MAX_DESCRIPTION_LENGTH)}...`
-                                                        : item.description} </Text>
-                                                <Text data-test={`menu-items=${item.price}`} fontSize={14} fontWeight="bold">${item.price.toFixed(2)}</Text>
+                                                        : item.description}
+                                                </Text>
+                                                <Text fontSize={14} fontWeight="bold">${item.price.toFixed(2)}</Text>
                                             </Box>
                                         </Link>
                                     ))}
@@ -100,8 +79,5 @@ export const Menu = () => {
                     </Box>
                 </Center>)}
         </Box>
-
-
     );
-
 };
